9b568f747bb2d6d3fcf1568155686bdcb9bb8627
Georgesa98 <georgesalebe0@gmail.com>
2023-12-15 17:16:42 +0200

    added fonts and SVGs put some starter code and added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div>asd</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="logo">
+          <img src="/shape.svg" width={324} id="shape" alt="" />
+          <img src="/Logo.svg" width={285} id="ge-logo" alt="" />
+        </div>
+        <div className="intro-text robotomono-bold-font">
+          <span className="html-tags robotomono-thin-font top-tag">{`<h1>`}</span>
+          <h1>Welcome to our frontend portfolio!</h1>
+          <span className="html-tags robotomono-thin-font bottom-tag">{`</h1>`}</span>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src="/Logo.svg" className="nav-item" width={64} alt="" />
+      <img src="/mobile/Hamburger.svg" className="nav-item" alt="" />
+    </nav>
+  );
+}
+
+export default Navbar;
